22470f1b158fbc126e2669d118599b502c18d8a5
aibrohim <ibrohim7840@gmail.com>
2021-03-22 12:17:03 +0500

    Slider is working. Gardener is shown.

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,13 @@ LANG_TOGGLER.addEventListener(`click`, () => {
   LANG_LIST.classList.toggle(`lang__list--open`);
 });
 
-var slider = tns();
+var slider = tns({
+  container: ".hero__slides",
+  autoplay: true,
+  // autoWidth: true,
+  controls: false,
+  navContainer: ".hero__slider-controls",
+  autoplay: true,
+  autoplayButtonOutput: false,
+  autoHeight: false,
+});
