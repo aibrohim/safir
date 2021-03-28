6138bb364887f7e9cbe34a1d988fbc48990445fc
aibrohim <ibrohim7840@gmail.com>
2021-03-29 00:14:10 +0500

    Products section done

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,4 @@
-const LANG_TOGGLER = document.querySelector(`.lang__toggler`);
-const LANG_LIST = document.querySelector(`.lang__list`);
-
-LANG_TOGGLER.addEventListener(`click`, () => {
-  LANG_LIST.classList.toggle(`lang__list--open`);
-});
-
-var slider = tns({
+const slider = tns({
   container: ".hero__slides",
   autoplay: true,
   // autoWidth: true,
