ef8b1a123f3d954da688633a300a717ea31ba732
aibrohim <ibrohim7840@gmail.com>
2021-03-30 23:09:45 +0500

    Added slider to meals section.

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,27 @@ const slider = tns({
   autoplayButtonOutput: false,
   autoHeight: false,
 });
+
+const mealsSlider = tns({
+  container: ".meals__list",
+  items: 4,
+  responsive: {
+    "1920": {
+      items: 4,
+      fixedWidth: 375
+    },
+    "1366": {
+      items: 4,
+      fixedWidth: 305
+    },
+    "992": {
+      items: 3
+    },
+    "768": {
+      items: 2
+    },
+    "576": {
+      items: 1
+    }
+  }
+})
