566d670086329d51d1744682142f3824d29db799
aibrohim <ibrohim7840@gmail.com>
2021-04-01 11:07:12 +0500

    Send request for getting instagram posts to facebook.

--- src/js/index.js
+++ src/js/index.js
@@ -36,7 +36,12 @@ const mealsSlider = tns({
   }
 });
 
-(async () => {
-  const instaData = await fetch(`https://graph.facebook.com/v10.0/instagram_oembed?url=https://www.instagram.com/p/CM_9xHbJtZM/`)
-  await console.log(instaData);
-})();
+// (async () => {
+//   const response = await fetch(
+//     `https://graph.facebook.com/v10.0/instagram_oembed?
+//       url=https://www.instagram.com/p/CM_9xHbJtZM/
+//       &access_token=195511135405769|bd29d7dd3df75e0616ff664ff36612e8
+//   `)
+//   const instaData = await response.json();
+//   console.log(instaData);
+// })();
