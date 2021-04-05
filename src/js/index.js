4a40acb4c1cec01a1d589beffa44d88d1bed0cb6
aibrohim <ibrohim7840@gmail.com>
2021-04-05 12:18:20 +0500

    Added about page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,73 +1,80 @@
-const slider = tns({
-  container: ".hero__slides",
-  autoplay: true,
-  // autoWidth: true,
-  controls: false,
-  navContainer: ".hero__slider-controls",
-  autoplay: true,
-  autoplayButtonOutput: false,
-  autoHeight: false,
-});
+const introSlides = document.querySelector(`.hero__slides`);
+const mealsSlides = document.querySelector(`.meals__list`);
 
-const mealsSlider = tns({
-  container: ".meals__list",
-  items: 4,
-  lazyload: true,
-  nav: false,
-  controlsContainer: "#meals__btns",
-  responsive: {
-    "1920": {
-      items: 4,
-      gutter: 40
-    },
-    "1366": {
-      items: 4,
-      gutter: 30
-    },
-    "992": {
-      items: 3
-    },
-    "768": {
-      items: 2
-    },
-    "576": {
-      items: 1
-    },
-    "0": {
-      items: 1
+if (introSlides && mealsSlides) {
+  const slider = tns({
+    container: ".hero__slides",
+    autoplay: true,
+    // autoWidth: true,
+    controls: false,
+    navContainer: ".hero__slider-controls",
+    autoplay: true,
+    autoplayButtonOutput: false,
+    autoHeight: false,
+  });
+
+  const mealsSlider = tns({
+    container: ".meals__list",
+    items: 4,
+    lazyload: true,
+    nav: false,
+    controlsContainer: "#meals__btns",
+    responsive: {
+      "1920": {
+        items: 4,
+        gutter: 40
+      },
+      "1366": {
+        items: 4,
+        gutter: 30
+      },
+      "992": {
+        items: 3
+      },
+      "768": {
+        items: 2
+      },
+      "576": {
+        items: 1
+      },
+      "0": {
+        items: 1
+      }
     }
-  }
-});
+  });
+}
 
 const articlesWrapper = document.querySelector(`.posts__wrapper`);
-const fragment = document.createDocumentFragment();
+if (articlesWrapper) {
+  const fragment = document.createDocumentFragment();
 
-const POSTS = [
-  `https://www.instagram.com/p/CNHAiRvplU-/`,
-  `https://www.instagram.com/p/CNFGXsXp2Ti/`,
-  `https://www.instagram.com/p/CNCcXIZpoiG/`
-];
+  const POSTS = [
+    `https://www.instagram.com/p/CNHAiRvplU-/`,
+    `https://www.instagram.com/p/CNFGXsXp2Ti/`,
+    `https://www.instagram.com/p/CNCcXIZpoiG/`
+  ];
 
-const CLIENT_MARKER = "bd29d7dd3df75e0616ff664ff36612e8";
-const APP_ID = "195511135405769";
+  const CLIENT_MARKER = "bd29d7dd3df75e0616ff664ff36612e8";
+  const APP_ID = "195511135405769";
 
-const ACCESS_TOKEN = APP_ID + "|" + CLIENT_MARKER;
+  const ACCESS_TOKEN = APP_ID + "|" + CLIENT_MARKER;
 
-const URL = `https://graph.facebook.com/v10.0/instagram_oembed`;
+  const URL = `https://graph.facebook.com/v10.0/instagram_oembed`;
 
-POSTS.forEach(async (post) => {
-  const response = await fetch(`${URL}?url=${post}&maxwidth=422&access_token=${ACCESS_TOKEN}&hidecaption=true`)
+  POSTS.forEach(async (post) => {
+    const response = await fetch(`${URL}?url=${post}&maxwidth=422&access_token=${ACCESS_TOKEN}&hidecaption=true`)
 
-  if (await response.status === 200) {
-    const instaData = await response.json();
+    if (await response.status === 200) {
+      const instaData = await response.json();
 
-    const article = document.createElement(`article`);
-    article.classList.add(`posts__item`);
+      const article = document.createElement(`article`);
+      article.classList.add(`posts__item`);
 
-    article.innerHTML = instaData.html;
-    articlesWrapper.append(article);
-  }
-});
+      article.innerHTML = instaData.html;
+      articlesWrapper.append(article);
+    }
+  });
+}
 
 const header = document.querySelector(`.main-header`);
 const burger = document.querySelector(`.hamburger`);
